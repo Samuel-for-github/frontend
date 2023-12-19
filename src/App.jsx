81d4aa71a4a202927537f099a4d0a40ad97f6f6e
Samuel fernandes <samuelfernandes10001@gamil.com>
2023-12-19 15:30:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+ const [intro, setIntro] = useState({})
+
+ useEffect(()=>{
+  axios.get("/api/intro")
+  .then((response)=>{
+    setIntro(response.data)
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+ })
+
+  return (
+    <>
+  <h1>I am</h1>
+  <span>{intro.name}</span>
+
+
+    </>
+  )
+}
+
+export default App
